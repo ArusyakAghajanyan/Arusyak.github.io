b167272164b20a05063741d3ea8d1415066f7159
ArusyakAghajanyan <ayad_aga@mail.ru>
2021-12-16 00:47:51 +0400

    fixed

---
+++ src/com/company/commons/constants/FileConstants.java
@@ -0,0 +1,8 @@
+package com.company.commons.constants;
+
+public class FileConstants {
+    public static final String IMAGE_RESOURCE_PATH = "./source/img/";
+    public static final String WHERE_PATH = "/Desktop";
+    public static final String FOLDER_NAME = "/Aya/Relq/Briefs/BriefTwo/Scripts";
+    public static final String USER_CV_DATA = "data.js";
+}
